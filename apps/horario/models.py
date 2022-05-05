0694d2b6af58e67b6a0509f48640ed7e50c987db
mfarn <maateus.n@gmail.com>
2022-05-05 18:26:46 -0300

    rota horario

--- apps/horario/models.py
+++ apps/horario/models.py
@@ -5,6 +5,6 @@ from ..prescricao.models import Prescricao
 
 class Horario(models.Model):
     id_horario = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    prescrisao_associada = models.ForeignKey(Prescricao, on_delete=models.CASCADE)
+    prescricao_associada = models.ForeignKey(Prescricao, on_delete=models.CASCADE)
     status_correspondente = models.CharField(max_length=1)
     horario = models.DateTimeField(auto_now=True)
