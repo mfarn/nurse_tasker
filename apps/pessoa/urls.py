7f3c9cbdf4f948ecae047fbfa289c75f1331293b
mfarn <maateus.n@gmail.com>
2022-05-08 10:23:42 -0300

    procfile added

--- apps/pessoa/urls.py
+++ apps/pessoa/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from . import views
 
 urlpatterns = [
-    path('usuarios', views.usuario_alt_view),
-    path('pacientes', views.paciente_alt_view),
+    path('usuarios/', views.usuario_alt_view),
+    path('pacientes/', views.paciente_alt_view),
     path('usuarios/<str:CPF>/', views.usuario_alt_view),
     path('pacientes/<str:CPF>/', views.paciente_alt_view),
 ]
