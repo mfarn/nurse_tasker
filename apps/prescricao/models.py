feb77f5d01538dd0ae17f419309a829d2b1f043e
PedroDidier <didiermaranhao@gmail.com>
2022-05-08 16:07:13 -0300

    horario previsto precisa ser preenchido

--- apps/prescricao/models.py
+++ apps/prescricao/models.py
@@ -7,7 +7,7 @@ class Prescricao(models.Model):
     id_prescricao = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     nome_droga = models.CharField(max_length=255)
     dosagem = models.CharField(max_length=255)
-    horario_previsto = models.DateTimeField(auto_now=True)
+    horario_previsto = models.DateTimeField()
     status_atual = models.CharField(max_length=1)
     cpf_paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     cpf_cadastrante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='cadastrante')
